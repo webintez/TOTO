b83f24f9c3ffb535b614e68c31f60dd5b258603b
unknown <webintez>
2026-01-14 15:20:27 +0530

    with vit

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,6 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-import Alpine from 'alpinejs';
 // import { Swiper, SwiperSlide } from 'swiper/vue'; // If using Vue, but we are using Blade/Alpine
 import Swiper from 'swiper';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
@@ -11,7 +10,4 @@ import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 // Configure Swiper
 Swiper.use([Navigation, Pagination, Autoplay]);
 
-window.Alpine = Alpine;
 window.Swiper = Swiper;
-
-Alpine.start();
